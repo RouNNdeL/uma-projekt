1dae89f562abafb7bb9f4c157fdfaedb6df64b07
Bartosz Walusiak <56489287+BWalusiak@users.noreply.github.com>
2022-01-16 18:16:48 +0100

    Add csv parser and generation with feedback (#1)
    * Add csv parser and generation with feedback
    
    * Remove junk, put data in data folder

---
+++ utils.py
@@ -0,0 +1,19 @@
+import csv
+
+import numpy as np
+
+
+def load_data_from_csv(path):
+    with open(path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        data = []
+        for row in csv_reader:
+            data.append(row)
+        return format_data(np.array(data, dtype=np.float64))
+
+
+def format_data(data):
+    # Format data, moving last value to the first position
+    data = np.roll(data, 1, axis=1)
+    return data
